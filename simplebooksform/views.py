68f232d5ce8b7286c030d667603ca3e91422d7c3
Daniel Kvist <daniel@danielkvist.net>
2013-05-02 09:08:02 +0200

    Added incoming/outgoing tax, needs refactoring

--- simplebooksform/views.py
+++ simplebooksform/views.py
@@ -53,23 +53,30 @@ def get_verifications(request):
 def result(request):
     expense = verification.objects.filter(account = 'expense').aggregate(Sum('amount'))
     income = verification.objects.filter(account = 'income').aggregate(Sum('amount'))    
-    tax6 = verification.objects.aggregate(Sum('tax6'))
-    tax12 = verification.objects.aggregate(Sum('tax12'))
-    tax25 = verification.objects.aggregate(Sum('tax25'))
-   
+    tax6in = verification.objects.filter(account = 'expense').aggregate(Sum('tax6'))
+    tax12in = verification.objects.filter(account = 'expense').aggregate(Sum('tax12'))
+    tax25in = verification.objects.filter(account = 'expense').aggregate(Sum('tax25'))
+    tax25out = verification.objects.filter(account = 'income').aggregate(Sum('tax25'))
+    
+    # TODO
+    # Refactor these requests to a single db query and pass in a better structure
+    # to the template.
     results = {}
     results['expense'] = xstr(expense['amount__sum'])
     results['income'] = xstr(income['amount__sum'])
-    results['tax6'] = xstr(tax6['tax6__sum'])
-    results['tax12'] = xstr(tax12['tax12__sum'])
-    results['tax25'] = xstr(tax25['tax25__sum'])
-    results['tax_total'] = string_to_int(results['tax6']) + string_to_int(results['tax12']) + string_to_int(results['tax25'])
-    results['subtotal'] = string_to_int(results['income']) - string_to_int(results['expense'])
+    results['tax6in'] = xstr(tax6in['tax6__sum'])
+    results['tax12in'] = xstr(tax12in['tax12__sum'])
+    results['tax25in'] = xstr(tax25in['tax25__sum'])
+    results['tax25out'] = xstr(tax25out['tax25__sum'])
+    results['tax_in_total'] = str_to_int(results['tax6in']) + str_to_int(results['tax12in']) + str_to_int(results['tax25in'])
+    results['tax_out_total'] = results['tax25out']
+    results['tax_subtotal'] = str_to_int(results['tax_out_total']) - results['tax_in_total']
+    results['subtotal'] = str_to_int(results['income']) - str_to_int(results['expense'])
     
     return render_to_response('result.html', {'results' : results})
     
 
-def string_to_int(s):
+def str_to_int(s):
     try:
         x = int(s)
     except ValueError:
